228774a0dca8ce8bf647c2b2333efdff55eaadfc
ToadetteHackFan <113904695+ToadetteHackFan@users.noreply.github.com>
2025-07-28 15:55:21 +0200

    Adjusted Packs

--- commands/hash.ts
+++ commands/hash.ts
@@ -6,36 +6,20 @@ import { fmtHex, getColor, makeRequest, resolveModRestrictPermission } from "../
 
 const config = getConfig();
 
-const RRID = 0x0A;
-const CTGPCID = 0x29C;
 const IKWID = 0x49;
-const LID = 0x29A;
-const OPID = 0x36B;
-const WTPID = 0x520;
+const NKWID = 0x00;
 
 export const PackOpts = [
-    { name: "Retro Rewind", value: RRID },
-    { name: "CTGP-C", value: CTGPCID },
     { name: "Insane Kart Wii", value: IKWID },
-    { name: "Luminous", value: LID },
-    { name: "OptPack", value: OPID },
-    { name: "WTP", value: WTPID }
+    { name: "Nexus Kart Wii", value: NKWID}
 ];
 
 export function packIDToName(packID: number) {
     switch (packID) {
-    case RRID:
-        return "Retro Rewind";
-    case CTGPCID:
-        return "CTGP-C";
     case IKWID:
         return "Insane Kart Wii";
-    case LID:
-        return "Luminous";
-    case OPID:
-        return "OptPack";
-    case WTPID:
-        return "WTP";
+    case NKWID:
+        return "Nexus Kart Wii";
     default:
         return "Unknown Pack";
     }
